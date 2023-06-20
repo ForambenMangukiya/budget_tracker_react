0b5f61696fd61956cd2197eef70c6e179005227b
Hani <hanikahhaleh@gmail.com>
2023-06-20 10:09:31 +0200

    deleted svg files and added png files to the public folder

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,8 @@
 export default function Dashboard() {
-    return (
-        <div>I'm in the Dashboard</div>
-    )
+  return (
+    <div>
+      I'm in the Dashboard
+      <img src="../../public/img/bills"></img>
+    </div>
+  );
 }
