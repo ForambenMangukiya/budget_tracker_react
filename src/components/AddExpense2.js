3f7d5ccd8f46c2b38384c3abd45d2e77521ada15
Anton <avonpoblozki@posteo.de>
2023-06-21 09:10:55 +0200

    bug fixes

--- src/components/AddExpense2.js
+++ src/components/AddExpense2.js
@@ -9,19 +9,63 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputAdornment from '@mui/material/InputAdornment';
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import "./styles/addexpense.css";
 import { Menu } from '@mui/material';
+import { CoPresent } from '@mui/icons-material';
 
 export default function AddExpense() {
   const [category, setCategory] = React.useState('');
   const [recurrence, setRecurrence] = React.useState('');
-  const [date, setDate] = React.useState('');
+  const [date, setDate] = React.useState();
   const [description, setDescription] = React.useState('');
   const [amount, setAmount] = React.useState();
+  const [error,setError] = React.useState(null)
+  const [isLoading,setIsLoading] = React.useState(null)
+
+  const handleSubmit = async () => {
+    console.log("category:",category)
+    console.log("recurrence:",recurrence)
+    console.log("date:",date)
+    console.log("description:",description)
+    console.log("amount:",amount)
+    try {
+        setIsLoading(true)
+        setError(false)
+        const res = await fetch("http://localhost:8080/transaction/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                category,
+                recurrence,
+                date,
+                description,
+                amount
+            }
+        })
+    } catch (error) {
+        setIsLoading(false)
+        setError(error)
+    }
+  }
 
   // const handleChange = (event) => {
   //   setAge(event.target.value);
   // };
+
+  //email: '3@4.de', password: 'Pw12345!'
+
+  //TO DOs
+  //Error Modal oder Error Nachricht einblenden
+  //Loading Spinnger hinzufügen
+  //Amount -> Type Number
+  //Logic Fetch Post
+  //text-align category/recurrence LEFT! 
 
   return (
     <>
@@ -49,53 +93,35 @@ export default function AddExpense() {
         
         {/*Date Page */}
         <FormControl fullWidth>
-        <InputLabel id="date-label">Date</InputLabel>
-        <Select required labelId="date-label" id="date" value={date} label="Date" onChange={e=>setDate(e.target.value)}>
-          <MenuItem value={"transport"}>Single Expense</MenuItem>
-          <MenuItem value={"food"}>Recurrent Expense</MenuItem>
-         </Select>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                
+            <DatePicker label="Date" inputFormat="DD/MM/YYYY" onChange={selectedDate=>setDate(selectedDate)}/* sx={{'& input': {color: 'black'}}} */ />
+            </LocalizationProvider>
+            {/* <InputLabel id="date-label">Date</InputLabel>
+            <Select required labelId="date-label" id="date" value={date} label="Date" onChange={e=>setDate(e.target.value)}>
+            <MenuItem value={"transport"}>Single Expense</MenuItem>
+            <MenuItem value={"food"}>Recurrent Expense</MenuItem>
+            </Select> */}
         </FormControl>
 
         {/*Description Page */}
         <FormControl fullWidth>
-        <TextField id="outlined-basic" label="Description" variant="outlined"  value={description}  onChange={e=>setDescription(e.target.value)}/>
-        {/* <InputLabel id="description-label">Description</InputLabel>
-        <Select labelId="description-label" id="description" value={description} label="Description" onChange={e=>setDescription(e.target.value)}>
-          <MenuItem value={"transport"}>Single Expense</MenuItem>
-          <MenuItem value={"food"}>Recurrent Expense</MenuItem>
-         </Select>
-         */}
+            <TextField id="outlined-basic" label="Description" variant="outlined"  value={description}  onChange={e=>setDescription(e.target.value)}/>
         </FormControl> 
         
         {/*Amount Page */}
-        {/* <FormControl fullWidth>
-        <InputLabel id="amount-label">Amount</InputLabel>
-        <TextField 
-            labelId="amount-label"
-          id="outlined-number"
-          
-          type="number"
-          value={amount}
-          onChange={e=>setAmount(e.target.value)}
-         
-        />
-         <InputLabel id="amount-label">Amount</InputLabel>
-        <Select labelId="amount-label" id="amount" value={amount} label="Amount" onChange={e=>setAmount(e.target.value)}>
-          <MenuItem value={"transport"}>Single Expense</MenuItem>
-          <MenuItem value={"food"}>Recurrent Expense</MenuItem>
-         </Select> 
-        </FormControl> */}
-
         <FormControl fullWidth>
           <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
           <OutlinedInput
             id="outlined-adornment-amount"
             startAdornment={<InputAdornment position="start">$</InputAdornment>}
             label="Amount"
+            onChange={e=>setAmount(e.target.value)}
+            value={amount}
           />
         </FormControl>
      
-        <Button variant="outlined" onClick={()=>setAmount()}>Button</Button>
+        <Button variant="outlined" onClick={()=>handleSubmit()}>Add Expense</Button>
      
     </Box>
 
