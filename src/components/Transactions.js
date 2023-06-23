a1cf4adc22f4b232554f60d4cfeeba6959529e0d
Anton <avonpoblozki@posteo.de>
2023-06-23 10:48:12 +0200

    UI setup

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,4 +1,7 @@
+import * as React from "react";
 import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -10,13 +13,82 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import InputAdornment from "@mui/material/InputAdornment";
 import CircularProgress from "@mui/material/CircularProgress";
 import Alert from "@mui/material/Alert";
+import Typography from "@mui/material/Typography";
 
+import { DataContext } from "../context/DataContext";
 import "./styles/transactions.css";
 
-import { AuthContext } from "../context/AuthContext";
+export default function Transaction() {
+  const [transaction, setTransaction] = React.useState(0);
 
-export default function Transactions() {
-  return <></>;
+  const handleChange = (event, newValue) => {
+    setTransaction(newValue);
+  };
+
+  const sampleData = [
+    { amount: "200.00€", category: "Food", date: "13.07.2023" },
+    { amount: "50.00€", category: "Transport", date: "13.07.2023" },
+    { amount: "3000.00€", category: "Rent", date: "10.07.2023" },
+    { amount: "2.99€", category: "Food", date: "10.07.2023" },
+    { amount: "56.46€", category: "Energy", date: "09.07.2023" },
+  ];
+  return (
+    <>
+      <Container className="transactions-container">
+        <Tabs value={transaction} onChange={handleChange} centered>
+          <Tab label="expenses" />
+          <Tab label="income" />
+        </Tabs>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            ml: 0.5,
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold", mb: 1 }}>Spent</Typography>
+        </Box>
+        {transaction === 0 ? (
+          <Box>
+            {sampleData.map((element) => (
+              <Box
+                component="div"
+                className="transaction-div"
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  variant="p"
+                  component="p"
+                  className="transaction-item"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  {element.amount}
+                </Typography>
+                <Typography
+                  variant="p"
+                  component="p"
+                  className="transaction-item"
+                >
+                  {element.category}
+                </Typography>
+                <Typography
+                  variant="p"
+                  component="p"
+                  className="transaction-item"
+                >
+                  {element.date}
+                </Typography>
+              </Box>
+            ))}
+          </Box>
+        ) : null}
+      </Container>
+    </>
+  );
 }
 
 // export default function Transactions() {
