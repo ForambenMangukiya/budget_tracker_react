01810df3ab7d5cc9f664f30906c546f06d881341
Anton <avonpoblozki@posteo.de>
2023-06-23 11:57:01 +0200

    merged expense ui with speed dial

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -12,16 +12,6 @@ import ManualEntry from "./svg/IconManuallyEnter";
 
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputAdornment from "@mui/material/InputAdornment";
-import CircularProgress from "@mui/material/CircularProgress";
-import Alert from "@mui/material/Alert";
 import Typography from "@mui/material/Typography";
 
 import { DataContext } from "../context/DataContext";
@@ -33,26 +23,7 @@ const actions = [
   { icon: <ManualEntry />, name: "Income", route: "/addincome" },
   { icon: <ScanReceipt />, name: "Scan", route: "/" },
 ];
-// export default function Transactions() {
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setIsLoading(true);
-//     setError(null);
 
-//     const response = await fetch("http://localhost:8080/transaction", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         category_name,
-//         tran_description,
-//         tran_amount,
-//         tran_sign,
-//         tran_curency,
-//         tran_date,
-//       }),
-//     });
-//     const data = await response.json();
-//   };
 export default function Transactions() {
   const [open, setOpen] = useState(false);
   const [transaction, setTransaction] = useState(0);
@@ -80,7 +51,7 @@ export default function Transactions() {
     { amount: "56.46€", category: "Energy", date: "09.07.2023" },
   ];
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="sm" className="transactions-container">
       <Tabs value={transaction} onChange={handleChange} centered>
         <Tab label="expenses" />
         <Tab label="income" />
