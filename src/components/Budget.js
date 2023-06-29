20eb98f1217902188f829d5f703246fbfa530ee8
Anton <avonpoblozki@posteo.de>
2023-06-29 10:52:11 +0200

    first init

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,35 +2,40 @@
 //   return <div>I'm in the Budget</div>;
 // }
 import React, { useState } from "react";
-import { SpeedDial, SpeedDialIcon, SpeedDialAction } from "@material-ui/lab";
-import AddIcon from "@material-ui/icons/Add";
-import { makeStyles } from "@material-ui/core/styles";
+// import { SpeedDial, SpeedDialIcon, SpeedDialAction } from "@material-ui/lab";
+// import AddIcon from "@material-ui/icons/Add";
+// import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  speedDialButton: {
-    width: 40,
-    height: 40,
-    position: "absolute",
-    left: 500,
-    top: 150,
-    backgroundColor: "#C80048",
-  },
-  customSpeedDialAction: {
-    width: 30,
-    height: 10,
-    position: "absolute",
-    left: 492,
-    top: 140,
-    backgroundColor: "#e6e6e6",
-  },
-  addBudgetText: {
-    fontSize: 12,
-    color: "black",
-  },
-}));
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import AddIcon from "@mui/icons-material/Add";
+
+// const useStyles = makeStyles((theme) => ({
+//   speedDialButton: {
+//     width: 40,
+//     height: 40,
+//     position: "absolute",
+//     left: 500,
+//     top: 150,
+//     backgroundColor: "#C80048",
+//   },
+//   customSpeedDialAction: {
+//     width: 30,
+//     height: 10,
+//     position: "absolute",
+//     left: 492,
+//     top: 140,
+//     backgroundColor: "#e6e6e6",
+//   },
+//   addBudgetText: {
+//     fontSize: 12,
+//     color: "black",
+//   },
+// }));
 
 export default function Budget() {
-  const classes = useStyles();
+  /* const classes = useStyles(); */
 
   const [open, setOpen] = useState(false);
 
@@ -60,22 +65,22 @@ export default function Budget() {
         open={open}
         // onOpen={handleSpeedDialOpen}
         // onClose={handleSpeedDialClose}
-        className={classes.speedDial}
-        FabProps={{
-          className: classes.speedDialButton,
-        }}
+        /* className={classes.speedDial} */
+        // FabProps={{
+        //   className: classes.speedDialButton,
+        // }}
       >
         <SpeedDialAction
           key="Add Budget"
           icon={<AddIcon />}
           onClick={handleAddBudget}
-          className={classes.customSpeedDialAction}
+          /* className={classes.customSpeedDialAction} */
         />
       </SpeedDial>
 
       {open && (
-        <div className={classes.addBudgetContainer}>
-          <p className={classes.addBudgetText}>Add Budget</p>
+        <div /* className={classes.addBudgetContainer} */>
+          <p /* className={classes.addBudgetText} */>Add Budget</p>
         </div>
       )}
 
